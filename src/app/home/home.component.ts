1fee927b1c9a719101fb11239a84a9505714e031
KhucSongNgan1984 <khucsongngan@gmail.com>
2023-02-13 10:57:48 +0700

    List new song Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SongsService} from "../service/songs/songs.service";
+
+import {Songs} from "../model/Songs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   slides: any = [
     '/assets/Untitled-1.jpg',
     '/assets/Untitled-2.jpg',
     '/assets/Untitled-3.jpg'
   ];
 
-  songs = new Array(10)
+  songs : Songs[] = []
+
+
+  constructor(private songService: SongsService) {
+
+  }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.songService.listNewSongsByDate().subscribe((data: Songs[]) => {
+      this.songs = data;
+    })
+  }
+
 }
